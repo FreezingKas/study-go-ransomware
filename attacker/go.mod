cf540f41222f925bb286245b1dc04c719fbee839
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2022-03-14 18:51:45 +0100

    Self contained tor

--- attacker/go.mod
+++ attacker/go.mod
@@ -6,10 +6,11 @@ require github.com/cretz/bine v0.2.0
 
 require (
 	github.com/ebfe/keccak v0.0.0-20150115210727-5cc570678d1b // indirect
+	github.com/gen2brain/go-libtor v1.2.0 // indirect
 	github.com/paxos-bankchain/moneroutil v0.0.0-20170611151923-33d7e0c11a62 // indirect
-	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70 // indirect
+	golang.org/x/crypto v0.0.0-20220313003712-b769efc7c000 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 )
 
 replace monero => ../monero
--- attacker/server.go
+++ attacker/server.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/cretz/bine/process/embedded"
 	"github.com/cretz/bine/tor"
+	"github.com/gen2brain/go-libtor"
 
 	"database/sql"
 	"monero"
@@ -23,32 +23,33 @@ func main() {
 }
 
 func run() error {
-	// Start tor with default config (can set start conf's DebugWriter to os.Stdout for debug logs)
-	fmt.Println("Starting and registering onion service, please wait a couple of minutes...")
-	t, err := tor.Start(nil, &tor.StartConf{ProcessCreator: embedded.NewCreator()})
+	// Start TOR
+	fmt.Println("Starting and registering onion service...")
+	t, err := tor.Start(context.TODO(), &tor.StartConf{ProcessCreator: libtor.Creator})
 	if err != nil {
-		log.Panicf("Unable to start Tor: %v", err)
+		log.Panicf("Failed to start tor: %v", err)
 	}
 	defer t.Close()
-	// Wait at most a few minutes to publish the service
+
+	// Wait to publish Service
 	listenCtx, listenCancel := context.WithTimeout(context.Background(), 3*time.Minute)
 	defer listenCancel()
-	// Create a v3 onion service to listen on any port but show as 80
+
+	// Listen
 	onion, err := t.Listen(listenCtx, &tor.ListenConf{Version3: true, RemotePorts: []int{80}})
 	if err != nil {
 		log.Panicf("Unable to create onion service: %v", err)
 	}
 	defer onion.Close()
-	fmt.Printf("Open Tor browser and navigate to http://%v.onion\n", onion.ID)
-	fmt.Println("Press enter to exit")
+
+	fmt.Printf("Listening for POST request on http://%v.onion\n", onion.ID)
+
 	// Serve the current folder from HTTP
 	errCh := make(chan error, 1)
-	go func() { errCh <- http.Serve(onion, http.FileServer(http.Dir("."))) }()
+	http.HandleFunc("/", listener)
+	errCh <- http.Serve(onion, nil)
 	// End when enter is pressed
-	go func() {
-		fmt.Scanln()
-		errCh <- nil
-	}()
+
 	if err = <-errCh; err != nil {
 		log.Panicf("Failed serving: %v", err)
 	}
